71aaa5ece192474bfabcbafa00aaaca1574a0748
Mat <mmateo95@protonmai.com>
2018-11-05 00:43:28 -0600

    cargar archivo a datagrid debo hacer archivo a matriz y luego matriz a datagrid

--- maquina virtual abc/maquina virtual abc/Form1.cs
+++ maquina virtual abc/maquina virtual abc/Form1.cs
@@ -50,9 +50,12 @@ namespace maquina_virtual_abc
 
             //lee los valores del archivo    
             string aux = richTextBox1.Text;
-            int posy = 0;
-            int posx = 0;
-            for (int x = 0; x < aux.Length; x++)
+
+            int dir_datos = 0;
+            int num_datos = 0;
+            int dir_codigo = 0;
+            int valor_leido = 0;
+            for (int x = 0; x < aux.Length || valor_leido==3; x++) //recorre el textbox
             {
                 int cont = 0;
                 int ini = x;
@@ -61,15 +64,105 @@ namespace maquina_virtual_abc
                     cont++;
                     x++;
                 }
+                if (valor_leido == 0)
+                    dir_datos = Convert.ToInt32(aux.Substring(ini, cont));              
+                if (valor_leido == 1)
+                    num_datos = Convert.ToInt32(aux.Substring(ini, cont));
+                if (valor_leido == 2)
+                    dir_codigo = Convert.ToInt32(aux.Substring(ini, cont));
+
+                valor_leido++;            
+            }
+
+
+            //guarda en memoria los datos      
+            int posx = 0;
+            int posy = dir_datos; //solo funciona si dir_datos es menor que 16
+            int z = 0;
+            int agregados = 0;
+
+            int k = 0;
+            int saltos = 0;
+              
+                while (k < aux.Length && saltos<3) //ve a la 4ta cantidad
+                {
+                   if(aux[k]=='\n')
+                    saltos++;
+
+                k++;
+                }
+            
+            for (int x = k; x < aux.Length && agregados<num_datos; x++) //recorre el textbox
+            {
+                int ini = x;
+                int cont = 0;
+           
+                    while (x < aux.Length && aux[x] != '\n')
+                    {
+                        cont++;
+                        x++;
+                    }
+                    z++;
+
                 dataGridView1.Rows[posx].Cells[posy].Value = (aux.Substring(ini, cont));
+                agregados++;
+                //se encarga de la coordenada en la matriz
                 posy++;
-                if(posy==16)
+                if (posy == 16)
                 {
                     posx++;
                     posy = 0;
                 }
             }
-            
+
+            //guarda en memoria las funciones   
+            int guardados = dir_codigo;
+            int guardados_aux = 0;
+            while(guardados>16)
+            {
+                guardados = guardados - 16;
+                guardados_aux++;
+            }
+            posy = guardados;
+            posx = guardados_aux; //solo funciona si dir_datos es menor que 16
+
+            z = 0;
+            agregados = 0;
+            k = 0;
+            saltos = 0;
+
+            while (k < aux.Length && saltos < 3+num_datos) //ve a la Nva cantidad
+            {
+                if (aux[k] == '\n')
+                    saltos++;
+
+                k++;
+            }
+
+            for (int x = k; x < aux.Length; x++) //recorre el textbox
+            {
+                int ini = x;
+                int cont = 0;
+
+                while (x < aux.Length && aux[x] != '\n')
+                {
+                    cont++;
+                    x++;
+                }
+                z++;
+
+                dataGridView1.Rows[posx].Cells[posy].Value = (aux.Substring(ini, cont));
+                //se encarga de la coordenada en la matriz
+                posy++;
+                if (posy == 16)
+                {
+                    posx++;
+                    posy = 0;
+                }
+            }
+
+
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
