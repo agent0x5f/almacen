f9147e5a67fa80d4869ba5bc0d2804b3c79fe75a
M <mmateo@protonmail.com>
2019-01-09 07:52:57 -0600

    conexion

--- proyectodba/proyectodba/Form1.Designer.cs
+++ proyectodba/proyectodba/Form1.Designer.cs
@@ -135,7 +135,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(767, 386);
+            this.tabPage2.Size = new System.Drawing.Size(790, 386);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
--- proyectodba/proyectodba/Form1.cs
+++ proyectodba/proyectodba/Form1.cs
@@ -7,15 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
 
 namespace proyectodba
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            //pruebo la conexion a la db
+            
+            //DATA SOURCE=localhost;PASSWORD=5695;PERSIST SECURITY INFO=True;USER ID=HR1
+            const string conexion = "DATA SOURCE=localhost;PASSWORD=5695;PERSIST SECURITY INFO=True;USER ID=HR1";
+            OracleConnection con = new OracleConnection(conexion);
+            con.Open();
+
+            string sql= "select usuario from cuentas where id = 1";
+            OracleCommand comando = new OracleCommand(sql,con);
+
+            OracleDataReader dr = comando.ExecuteReader();
+            dr.Read();
+            MessageBox.Show(dr[0].ToString());
+
+            con.Dispose();
+            
         }
+        
 
         private void Boton_ok_Click(object sender, EventArgs e)
         {
